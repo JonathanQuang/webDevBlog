a601ccba48a21e088eda6f7fe8e2c1b37645a739
unknown <ataradachuk@gmail.com>
2017-10-29 14:00:42 -0400

    sqlite connect done

--- blog.py
+++ blog.py
@@ -17,24 +17,24 @@ def root():
 @myapp.route('/home/', methods = ['GET','POST'])
 def home():
     if bool(session) != False:
-        return render_template("listUserEntries.html", USER = session['user'])
+        return render_template("listUsers.html", USER = session['user'])
     user = request.form['username']
     print user
     password = hash(request.form['inputPassword3'])
     print password
     if request.form['up'] == "Sign up":
-        print DBbuild.listUsers("users", False, "")[0][0]
-        if (user in DBbuild.listUsers("users", False, "")):
-            return redirect(url_for('error'))
+        for entry in DBbuild.listUsers("users", False, ""):
+            if (user == entry[0]):
+                return redirect(url_for('error'))
         DBbuild.insertintoTABLE('users', user, password)
         session['user'] = user
         session['pass'] = password
-        return render_template('listUserEntries.html', USER = session['user'])
+        return render_template('listUsers.html', USER = session['user'])
     if request.form['in'] == "Log In":
         if not (user in DBbuild.listUsers("users", False, "")):
             return redirect(url_for('error'))
         if password == hash(listUsers("users", True, user)[0]):
-            return render_template('listUserEntries.html', USER = session['user'])
+            return render_template('listUsers.html', USER = session['user'])
        
 @myapp.route('/error/', methods = ['GET','POST'])
 def error():
